fc9c744c7e87782dd848305fcafca58c0999d2ca
Rile-Cry <52363024+Rile-Cry@users.noreply.github.com>
2024-11-05 22:58:48 -0500

    Added Instant Mesanger
    Made the MSN look a little better and added in the IM.

---
+++ scenes/programs/im.gd
@@ -0,0 +1,12 @@
+extends Window
+
+# TODO Implement Messaging system, prolly will use a json to read from and a number to keep track.
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scenes/programs/msn.gd
+++ scenes/programs/msn.gd
@@ -1,24 +1,42 @@
 extends Window
 
+# TODO Move user statuses to Globals
 var users:= {
-	"Marcus": true,
-	"Paula": true,
-	"Jingo": false,
+	"Marcus": [true, null],
+	"Paula": [true, null],
+	"Jingo": [false, null],
 }
+
+@onready var offline_label := $PanelContainer/VBoxContainer/UserBox/MarginContainer/VBoxContainer/ScrollContainer/UserContainer/OfflineLabel
+@onready var user_list := $PanelContainer/VBoxContainer/UserBox/MarginContainer/VBoxContainer/ScrollContainer/UserContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	connect("close_requested", close_requested)
+	
 	init_online()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	check_online()
+	pass
 
 
 func init_online() -> void:
-	pass
+	for user in users:
+		var user_label = Label.new()
+		user_label.text = user
+		users[user][1] = user_label
+		if users[user][0]:
+			var i = offline_label.get_index()
+			user_list.add_child(user_label)
+			user_list.move_child(offline_label, i + 1)
+		else:
+			user_list.add_child(user_label)
 
 
 func check_online() -> void:
 	pass
+
+func close_requested() -> void:
+	queue_free()
