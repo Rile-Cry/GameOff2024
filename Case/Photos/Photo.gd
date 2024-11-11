946ee421c4bc1262d641c8f9ec984c79a51973e8
DarrylBD99 <darrylhoBD@gmail.com>
2024-11-11 10:46:38 +0800

    Added Case and Photos resource

---
+++ Case/Photos/Photo.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Photo
+
+@export var name : String
+@export var time : String
+
+@export var texture : Texture2D
+@export var glitched : bool = false
+@export var is_location : bool = true
+@export var scene : PackedScene
