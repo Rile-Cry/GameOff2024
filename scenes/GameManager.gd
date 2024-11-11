d51e0ef9e8fab7dde3c11c1a69b329784d2a432c
DarrylBD99 <darrylhoBD@gmail.com>
2024-11-12 01:01:44 +0800

    updated mission book graphics

--- scenes/GameManager.gd
+++ scenes/GameManager.gd
@@ -1,1 +1,3 @@
+extends Node
+
 var current_case : Case
--- ui/MissionBook.gd
+++ ui/MissionBook.gd
@@ -1,26 +1,22 @@
 extends Control
 
 @export var open_b : Button
-@export var anim : AnimationPlayer
+@export var close_b : Button
+@export var tab_container : TabContainer
 
 var is_open = false
 
 func _ready():
-	open_b.button_down.connect(open)
+	open_b.button_down.connect(open_close)
+	close_b.button_down.connect(open_close)
+	self.visible = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		close()
+		tab_container.current_tab = 0
+		open_close()
 
-func open():
-	if not is_open:
-		anim.play("open")
-		is_open = true
-		open_b.visible = false
-
-func close():
-	if is_open:
-		anim.play("close")
-		is_open = false
-		await anim.animation_finished
-		open_b.visible = true
+func open_close():
+	is_open = not is_open
+	self.visible = is_open
+	open_b.visible = not is_open
