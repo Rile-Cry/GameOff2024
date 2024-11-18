bda9cc1bc6dd697f9c240597a1bff442ee4934c0
DarrylBD99 <darrylhoBD@gmail.com>
2024-11-18 22:23:06 +0800

    fixed pressed and focus being default button

--- Case/mission_book_node.gd
+++ Case/mission_book_node.gd
@@ -15,10 +15,12 @@ func _ready() -> void:
 	style_texture.texture = resource.texture_icon
 	style_texture.set_texture_margin_all(2)
 	add_theme_stylebox_override("normal", style_texture)
+	add_theme_stylebox_override("pressed", style_texture)
 	
 	style_texture_hover = style_texture.duplicate()
 	style_texture_hover.set_texture_margin_all(0)
 	add_theme_stylebox_override("hover", style_texture_hover)
+	add_theme_stylebox_override("focus", style_texture_hover)
 	
 	size_flags_horizontal = SizeFlags.SIZE_EXPAND_FILL
 	custom_minimum_size.y = 76
