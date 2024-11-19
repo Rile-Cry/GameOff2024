271e0a4014875e833088e6aeb2d541f8f8810890
DarrylBD99 <darrylhoBD@gmail.com>
2024-11-19 10:49:47 +0800

    fixed errors

--- scenes/GameManager/audio.gd
+++ scenes/GameManager/audio.gd
@@ -2,7 +2,7 @@ extends AudioStreamPlayer
 
 
 # Called when the node enters the scene tree for the first time.
-func play_audio(play_audio : String):
-	self["parameters/switch_to_clip"] = play_audio
+func play_audio(audio_name : String):
+	self["parameters/switch_to_clip"] = audio_name
 	play()
 	
