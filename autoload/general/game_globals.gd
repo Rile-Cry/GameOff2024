73d28c8af35a2a48717bc5b9b319a404a8fd1ae5
Rile-Cry <52363024+Rile-Cry@users.noreply.github.com>
2024-11-23 21:43:08 -0500

    IM, MSN, and Vault Redux
    Made the IM and MSN work a little better, and also switched console out for "Vault" which is where players can enter a code for various cheats and effects.

--- autoload/general/game_globals.gd
+++ autoload/general/game_globals.gd
@@ -5,17 +5,6 @@ var current_case : Case
 var typing_speed : int = 20
 #endregion
 
-
-#region Physics layers
-const world_collision_layer: int = 1
-const player_collision_layer: int = 2
-const enemies_collision_layer: int = 4
-const hitboxes_collision_layer: int = 8
-const shakeables_collision_layer: int = 16
-const interactables_collision_layer: int = 32
-#endregion
-
-
 #region General helpers
 func delay_func(callable: Callable, time: float, deferred: bool = true):
 	if callable.is_valid():
