754988cb86c3af52daebbf3e4f1334dac4933b03
DarrylBD99 <darrylhoBD@gmail.com>
2024-11-30 23:05:44 +0800

    Update audio.gd

--- scenes/GameManager/audio.gd
+++ scenes/GameManager/audio.gd
@@ -5,7 +5,6 @@ extends AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func play_audio(audio_name : String, volume_db_ : float = 0.0):
 	volume_db = volume_db_
-	
 	if overlap or self["parameters/switch_to_clip"] != audio_name:
 		self["parameters/switch_to_clip"] = audio_name
 		play()
