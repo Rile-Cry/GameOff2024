070dedeeac166424c419e50144d32d8c32a0ba37
DarrylBD99 <darrylhoBD@gmail.com>
2024-12-01 17:52:40 +0800

    Update InterrogationRoom.gd

--- scenes/locations/InterrogationRoom.gd
+++ scenes/locations/InterrogationRoom.gd
@@ -4,8 +4,13 @@ func _ready() -> void:
 	if UIManager:
 		UIManager.show_shader("vignette")
 	
+	$FinaleActors.hide()
+	
 	GlobalGameEvents.connect("scene_loaded", _scene_loaded)
-	super()
+	if GameManager.global_variables["all_clues"]:
+		actor.show()
+	else:
+		super()
 
 func _get_clue_location(clue : Clue) -> bool:
 	if await super(clue):
@@ -20,25 +25,37 @@ func _notification(what):
 			UIManager.hide_shader("vignette")
 
 func dialogue_start_action(idx : int):
-	if idx == 0:
-		if BgmAudio and BgmAudio.playing:
-			BgmAudio.stop()
-		if AmbientAudio and AmbientAudio.playing:
-			AmbientAudio.stop()
-		
-		if UIManager:
-			UIManager.anim_player.play("hide_room")
-			await UIManager.anim_player.animation_finished
-		actor.show()
-	elif idx == 1:
-		if BgmAudio and not bgm.is_empty():
-			BgmAudio.play_audio(bgm)
-		if AmbientAudio and not ambiance.is_empty():
-			AmbientAudio.play_audio(ambiance)
-		
-		if UIManager:
-			UIManager.anim_player.play("reveal_room")
-			await UIManager.anim_player.animation_finished
+	if GameManager.global_variables["all_clues"]:
+		if idx == 0:
+			
+			
+			if UIManager:
+				if BgmAudio and not bgm.is_empty():
+					BgmAudio.play_audio(bgm)
+				if AmbientAudio and not ambiance.is_empty():
+					AmbientAudio.play_audio(ambiance)
+				UIManager.anim_player.play("reveal_room")
+				await UIManager.anim_player.animation_finished
+	else:
+		if idx == 0:
+			if BgmAudio and BgmAudio.playing:
+				BgmAudio.stop()
+			if AmbientAudio and AmbientAudio.playing:
+				AmbientAudio.stop()
+			
+			if UIManager:
+				UIManager.anim_player.play("hide_room")
+				await UIManager.anim_player.animation_finished
+			actor.show()
+		elif idx == 1:
+			if BgmAudio and not bgm.is_empty():
+				BgmAudio.play_audio(bgm)
+			if AmbientAudio and not ambiance.is_empty():
+				AmbientAudio.play_audio(ambiance)
+			
+			if UIManager:
+				UIManager.anim_player.play("reveal_room")
+				await UIManager.anim_player.animation_finished
 
 func _scene_loaded() -> void:
 	if GameManager.global_variables["all_clues"]:
