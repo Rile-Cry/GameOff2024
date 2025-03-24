b98a0e04c986861311a3007151a9a742eb18a205
Rile-Cry <rileycriddle@yahoo.com>
2025-03-24 08:49:03 -0400

    File Cleanup
    Removing a bunch of files that I find unnecessary

--- Case/Clues/CluesResource.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name Clue
-
-@export var id : String
-@export var name : String
-@export var description : String
-@export var location : String
-@export var texture : Texture2D
-@export var texture_icon : Texture2D
---
+++ components/case/clues/clue_resource.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name Clue
+
+@export var clue_id : String
+@export var clue_name : String
+@export var clue_description : String
+@export var clue_location : String
+@export var clue_texture : Texture2D
+@export var clue_texture_icon : Texture2D
